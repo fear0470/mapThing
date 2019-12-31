0319d755753d0a63b10d65221ea993b30e608dd6
Nicholas Fearns <you@example.com>
2019-12-31 23:26:08 +0000

    testing send request with postman, switched to port 3000

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+app.get('/api/v1/stores', (req, res) => {
+    res.send('Hello ');
+});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
