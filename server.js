e9820481ace6690a1514b23a451379282d32b2bb
Nicholas Fearns <you@example.com>
2019-12-31 23:16:13 +0000

    settinf up express

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
